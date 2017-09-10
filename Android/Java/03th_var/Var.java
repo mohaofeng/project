aa575ab9368cf87fa0d34b16446bf96bca52f98e
mohaofeng <859566883.qq.com>
2017-09-11 00:52:53 +0800

    java数据类型_第三课

---
+++ Android/Java/03th_var/Var.java
@@ -0,0 +1,27 @@
+
+
+
+public class Var{
+	public static void main(String args[]){
+		int a = 3;
+		short s2 = 4;
+		//short s3 = 40000;
+		short s4 = (short)40000;
+		float f = (float)3.14;
+		float f2 = 3.14f;
+		
+		/* java has no point */
+		//int* p= malloc(10*sizeof(int));
+		int p[]= new int[10];
+		int p2[]= {1,2,3};
+		
+		/* java cha 2byte */
+		//char str[100];
+		char str[] = new char[100];
+		s2 = s2 +1;
+		//char str2[] = "abc";
+		String  str2 = "abc";
+		
+		
+	}
+}
